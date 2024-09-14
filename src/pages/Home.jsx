30154af5959476513193a7e0830406041f31265a
asheq1 <bsmrstu18fmb025@gmail.com>
2024-09-14 17:40:55 +0600

    navber

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav />
+            <h2>This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
